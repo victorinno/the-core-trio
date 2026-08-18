24081e2fc2ee0fb19087be801b5d2a2eb6ad2705
victorinno <1.300915e+06+victorinno@users.noreply.github.com>
2026-08-18 18:01:57 +0000

    deploy: publish individual prologue backgrounds

---
+++ assets/rgbdDecode.fragment-DXm8TxUG.js
@@ -0,0 +1,5 @@
+import{u as e}from"./index-B2yp5jix.js";import{h as t}from"./helperFunctions-C_0cfdjz.js";const o="rgbdDecodePixelShader",n=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const a=[t];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const i={name:o,shader:n};export{i as rgbdDecodePixelShader};
