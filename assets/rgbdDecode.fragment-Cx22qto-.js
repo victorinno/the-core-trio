7b97631a2fbe0f565643e1a56dae20bbbc5271f5
victorinno <1.300915e+06+victorinno@users.noreply.github.com>
2026-08-18 18:34:13 +0000

    deploy: publish corrected prologue scenes

--- assets/rgbdDecode.fragment-Cx22qto-.js
+++
@@ -1,5 +0,0 @@
-import{u as e}from"./index-CoFveo_D.js";import{h as n}from"./helperFunctions-DxGc4lqo.js";const t="rgbdDecodePixelShader",a=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;
-#include<helperFunctions>
-#define CUSTOM_FRAGMENT_DEFINITIONS
-@fragment
-fn main(input: FragmentInputs)->FragmentOutputs {fragmentOutputs.color=vec4f(fromRGBD(textureSample(textureSampler,textureSamplerSampler,input.vUV)),1.0);}`;e.ShadersStoreWGSL[t]||(e.ShadersStoreWGSL[t]=a);const S=[n];for(const r of S)e.IncludesShadersStoreWGSL[r.name]||(e.IncludesShadersStoreWGSL[r.name]=r.shader);const m={name:t,shader:a};export{m as rgbdDecodePixelShaderWGSL};
