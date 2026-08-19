e8febba217beccc0caa641984a5b04c4421f9b40
victorinno <1.300915e+06+victorinno@users.noreply.github.com>
2026-08-19 10:05:00 +0000

    deploy: location-led navigation UX

--- assets/rgbdDecode.fragment-BrJUr45u.js
+++
@@ -1,5 +0,0 @@
-import{u as e}from"./index-CAEna2gR.js";import{h as t}from"./helperFunctions-6rPjDt1b.js";const o="rgbdDecodePixelShader",n=`varying vec2 vUV;uniform sampler2D textureSampler;
-#include<helperFunctions>
-#define CUSTOM_FRAGMENT_DEFINITIONS
-void main(void) 
-{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const a=[t];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const i={name:o,shader:n};export{i as rgbdDecodePixelShader};
---
+++ assets/rgbdDecode.fragment-DHx1FdUn.js
@@ -0,0 +1,5 @@
+import{u as e,v as a}from"./index-CBYVBv3k.js";const o="rgbdDecodePixelShader",n=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const d=[a];for(const r of d)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const s={name:o,shader:n};export{s as rgbdDecodePixelShader};
